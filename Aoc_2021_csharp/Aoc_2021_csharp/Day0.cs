f61e5f5916b72153837dfee2ce6f8e3a3154338f
Aleks Pk <fatalitytnt@gmail.com>
2023-12-31 07:53:04 +0100

    2021: day0 (template)

---
+++ Aoc_2021_csharp/Aoc_2021_csharp/Day0.cs
@@ -0,0 +1,16 @@
+﻿namespace Aoc_2021_csharp;
+
+public class Day0
+{
+    public long SolvePart1()
+    {
+        var a = 10.DayInput();
+        return a.Length;
+    }
+
+    public long SolvePart2()
+    {
+        var a = 10.DayInput();
+        return a.Length;
+    }
+}
